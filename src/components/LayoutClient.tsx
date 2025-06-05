355ce982ca59dd552e72c94d784f831d1222a253
Gabriel Santi <santidev95@gmail.com>
2025-06-05 17:54:22 -0400

    Feat/layout update (#7)
    * feat: portfilio
    
    * feat: docs

--- src/components/LayoutClient.tsx
+++ src/components/LayoutClient.tsx
@@ -3,7 +3,7 @@ import { usePathname } from "next/navigation";
 import ContextProvider from "@/context";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaXTwitter, FaBook } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 
 export default function LayoutClient({ children, cookies }: { children: React.ReactNode; cookies: string | null }) {
@@ -22,7 +22,10 @@ export default function LayoutClient({ children, cookies }: { children: React.Re
         <Toaster position="bottom-center" />
         {!hideNavAndFooter && (
           <footer className="w-full text-center text-sm py-2 absolute bottom-0">
-            <span className="text-indigo-300 font-mono flex items-center justify-center gap-2">    
+            <span className="text-indigo-300 font-mono flex items-center justify-center gap-2"> 
+            <a href="https://montools.xyz/docs" target="_blank" rel="noopener noreferrer">
+                  <FaBook className="text-indigo-300" />  
+              </a>   
               <a href="https://github.com/SantiSjp/mon-tools" target="_blank" rel="noopener noreferrer">
                   <FaGithub className="text-indigo-300" />  
               </a>          
