e9ce4ee731b398da0c06cbfdf3bb9d8f4c3e492e
Gabriel Santi <santidev95@gmail.com>
2025-06-17 19:55:35 -0400

    chore: add loog

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,21 @@
-// middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
 
 const INTERNAL_API_PREFIX = '/api/monorail/';
 const ALLOWED_ORIGIN = 'https://testnet.montools.xyz';
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  const referer = req.headers.get("referer") || "";
-  const origin = req.headers.get("origin") || "";
+    const { pathname } = req.nextUrl;
+    const referer = req.headers.get("referer") || "";
+    const origin = req.headers.get("origin") || "";
 
-  const isInternalAPI = pathname.startsWith(INTERNAL_API_PREFIX);
+    const isInternalAPI = pathname.startsWith(INTERNAL_API_PREFIX);
 
-  if (isInternalAPI && !(referer.startsWith(ALLOWED_ORIGIN) || origin.startsWith(ALLOWED_ORIGIN))) {
-    return new NextResponse("Forbidden", { status: 403 });
-  }
+    if (isInternalAPI && !(referer.startsWith(ALLOWED_ORIGIN) || origin.startsWith(ALLOWED_ORIGIN))) {
+        console.info(`[Middleware] Request to ${req.nextUrl.pathname} from ${referer} or ${origin} is forbidden`);
+        return new NextResponse("Forbidden", { status: 403 });
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
 }
 
 export const config = {
